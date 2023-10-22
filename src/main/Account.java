29fb2c4d3c54baad9a99293df494e00c9f9c9c68
hoya <sv05159@naver.com>
2023-10-22 22:57:23 +0900

    [은행 계좌(Account) 클래스 만들기] 두 번째 질문: 잔고 조회
    - TDD의 부가효과: 별 생각 없이 만들었던 테스트 케이스 자체가, "Account 클래스의 구조에 대해 조금 생각해보라"고 한다.
    - 오류(예외가 발생해서 테스트 중단)와 실패(테스트 조건식을 만족시키지 못함)

--- src/main/Account.java
+++ src/main/Account.java
@@ -1,4 +1,11 @@
 package main;
 
 public class Account {
+    public Account(int i) {
+
+    }
+
+    public int getBalance() {
+        return 0;
+    }
 }
--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -3,10 +3,24 @@ package test;
 import main.Account;
 import org.junit.Test;
 
+import static org.junit.Assert.fail;
+
 public class AccountTest {
 
     @Test
     public void testAccount() throws Exception {
-        Account account = new Account();
+        //초기 입금액을 명시적으로 설정하지 않고는 계좌 생성 불가
+        Account account = new Account(10000);
+    }
+
+    @Test
+    public void testGetBalance() throws Exception {
+        //10000원으로 계좌 생성
+        Account account = new Account(10000);
+
+        // 잔고 조회 결과 일치
+        if (account.getBalance() != 10000) {
+            fail();
+        }
     }
 }
