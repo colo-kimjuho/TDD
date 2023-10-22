9705ed87499297afcfdbbb2f22c3bbf13d22291c
hoya <sv05159@naver.com>
2023-10-22 22:43:18 +0900

    [은행 계좌(Account) 클래스 만들기] 최초의 정제
    1. 리펙토링
    ```
    - 소스의 가독성이 적절한가?
    - 중복된 코드는 없는가?
    - 이름이 잘못 부여된 메소드나 변수명은 없는가?
    - 구조의 개선이 필요한 부분은 없는가?
    ```
    
    -> JUnit 단위 테스트 프레임워크
    - main 메서드, if문 삭제
    - 코드 간결

--- src/test/AccountTest.java
+++ src/test/AccountTest.java
@@ -1,24 +1,12 @@
 package test;
 
 import main.Account;
+import org.junit.Test;
 
 public class AccountTest {
 
+    @Test
     public void testAccount() throws Exception {
         Account account = new Account();
-        if (account == null) {
-            throw new Exception("계좌생성 실패");
-        }
-    }
-
-    public static void main(String[] args) {
-        AccountTest test = new AccountTest();
-        try {
-            test.testAccount();
-        } catch (Exception e) {
-            System.out.println("실패(X)");
-            e.printStackTrace();
-        }
-        System.out.println("성공(O)");
     }
 }
